281dacf7f49a14ce250b6d7417b9330d0c11d196
gyanavardhana <89439095+gyanavardhana@users.noreply.github.com>
2022-10-11 22:41:46 +0530

    #include<stdio.h> int main(){     int n,i,a[23],j=0;     scanf("%d",&n);     for(i=0; i<n; i++){         scanf("%d ",&a[i]);         if (a[i]%2!=0){             j++;         }     }     if(j<3){         printf("YES");     }else{         printf("NO");     }     return 0; }

---
+++ Can_Robber_Escape?.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main(){
+    int n,i,a[23],j=0;
+    scanf("%d",&n);
+    for(i=0; i<n; i++){
+        scanf("%d ",&a[i]);
+        if (a[i]%2!=0){
+            j++;
+        }
+    }
+    if(j<3){
+        printf("YES");
+    }else{
+        printf("NO");
+    }
+    return 0;
+}
